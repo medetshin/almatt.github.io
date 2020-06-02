0bd4f3a347d76c2d8a6b0d6ddecac25dd7cba790
Almat Karibay <karibayevalmat@gmail.com>
2020-06-02 18:14:25 +0600

    -=-

--- a.js
+++ a.js
@@ -1,1 +1,10 @@
-alert('aaa');
+// Create a class for the element
+class MyFrame extends HTMLIFrameElement {
+  constructor() {
+    self = super();
+    console.log('AAAAAA')
+    self.setAttribute('allowpaymentrequest', 'true');
+  }
+}
+
+customElements.define('my-frame', MyFrame, { extends: 'iframe' });
